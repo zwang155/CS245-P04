5e3ce4e658e9ec34dbecfca38e15b5b4f6c4dd7c
David Guy Brizan <dbrizan@gmail.com>
2017-09-21 18:28:31 -0700

    Incorrect Queue/Stack implementations accounted

--- Practice4Test.java
+++ Practice4Test.java
@@ -48,82 +48,87 @@ public class Practice4Test {
 	public void runTest() {
 		// Theory: The Queue and Stack constructors have been called.
 		int grade = 0;
-		// If the stack and queue are empty (default), that's a milestone.
-		if (stack.empty())
-			grade += 5;
-		if (queue.empty())
-			grade += 5;
-
-		// First tests: is the queue correct? Is the stack correct?
-		String first = "first";
-		String second = "second";
-		queue.enqueue(first);
-		queue.enqueue(second);
-		if (queue.dequeue() == first && queue.dequeue() == second && queue.empty()) {
-			System.out.println("[+10%] Queue functions appear correct.");
-			grade += 10;
-		} else {
-			System.out.println("[ XX ] One or more Queue functions appear incorrect.");
-		}
-		
-		stack.push(first);
-		stack.push(second);
-		if (stack.pop() == second && stack.pop() == first && stack.empty()) {
-			System.out.println("[+10%] Stack functions appear correct.");
-			grade += 10;
-		} else {
-			System.out.println("[ XX ] One or more Stack functions appear incorrect.");
-		}
-		
-		// Additional sanity test: place a large number of items in the stack / queue to see if it resizes.
-		for (int i = 0; i < size_of_test; i++) {
-			queue.enqueue(Integer.MAX_VALUE);
-			stack.push(Integer.MIN_VALUE);
-		}
-		boolean emptiedEarly = false;
-		for (int i = 0; i < size_of_test; i++) {
-			if ( queue.empty() || stack.empty()) {
-				emptiedEarly = true;
-			}
-			queue.dequeue();
-			stack.pop();
-		}
-		if (queue.empty() && stack.empty() && ! emptiedEarly) {
-			System.out.println("[+10%] Stack and Queue appear to resize correctly.");
-			grade += 10;
-		} else {
-			System.out.println("[ XX ] Stack or Queue do not resize correctly.");
-		}
-		
-		System.out.println("====================");
-
-		// Second tests: does it correctly find palindromes?
-		for (String expression : expressionList) {
-			if (isPalindrome(expression)) {
-				System.out.println("[+10%] \"" + expression + "\" is a palindrome (correct).");
+		try {
+			// If the stack and queue are empty (default), that's a milestone.
+			if (stack.empty())
+				grade += 5;
+			if (queue.empty())
+				grade += 5;
+			System.out.println("[+" + grade + "%] Queue and Stack declared correctly.");
+	
+			// First tests: is the queue correct? Is the stack correct?
+			String first = "first";
+			String second = "second";
+			queue.enqueue(first);
+			queue.enqueue(second);
+			if (queue.dequeue() == first && queue.dequeue() == second && queue.empty()) {
+				System.out.println("[+10%] Queue functions appear correct.");
 				grade += 10;
+			} else {
+				System.out.println("[ XX ] One or more Queue functions appear incorrect.");
 			}
-			else {
-				System.out.println("[ XX ] \"" + expression + "\" determined to be a non-palindrome (incorrect).");
-			}
-		}
-		
-		System.out.println("====================");
-
-		// Third tests: can it correctly identify non-palindromes?
-		for (String expression : wrongList) {
-			// System.out.print(expression + " determined to be: ");
-			if (isPalindrome(expression)) {
-				System.out.println("[ XX ] \"" + expression + "\" determined to be a palindrome (incorrect).");
-			}
-			else {
-				System.out.println("[+10%] \"" + expression + "\" is a non-palindrome (correct)");
+			
+			stack.push(first);
+			stack.push(second);
+			if (stack.pop() == second && stack.pop() == first && stack.empty()) {
+				System.out.println("[+10%] Stack functions appear correct.");
 				grade += 10;
+			} else {
+				System.out.println("[ XX ] One or more Stack functions appear incorrect.");
 			}
+			
+			// Additional sanity test: place a large number of items in the stack / queue to see if it resizes.
+			for (int i = 0; i < size_of_test; i++) {
+				queue.enqueue(Integer.MAX_VALUE);
+				stack.push(Integer.MIN_VALUE);
+			}
+			boolean emptiedEarly = false;
+			for (int i = 0; i < size_of_test; i++) {
+				if ( queue.empty() || stack.empty()) {
+					emptiedEarly = true;
+				}
+				queue.dequeue();
+				stack.pop();
+			}
+			if (queue.empty() && stack.empty() && ! emptiedEarly) {
+				System.out.println("[+10%] Stack and Queue appear to resize correctly.");
+				grade += 10;
+			} else {
+				System.out.println("[ XX ] Stack or Queue do not resize correctly.");
+			}
+			
+			System.out.println("====================");
+	
+			// Second tests: does it correctly find palindromes?
+			for (String expression : expressionList) {
+				if (isPalindrome(expression)) {
+					System.out.println("[+10%] \"" + expression + "\" is a palindrome (correct).");
+					grade += 10;
+				}
+				else {
+					System.out.println("[ XX ] \"" + expression + "\" determined to be a non-palindrome (incorrect).");
+				}
+			}
+			
+			System.out.println("====================");
+	
+			// Third tests: can it correctly identify non-palindromes?
+			for (String expression : wrongList) {
+				// System.out.print(expression + " determined to be: ");
+				if (isPalindrome(expression)) {
+					System.out.println("[ XX ] \"" + expression + "\" determined to be a palindrome (incorrect).");
+				}
+				else {
+					System.out.println("[+10%] \"" + expression + "\" is a non-palindrome (correct)");
+					grade += 10;
+				}
+			}
+		} catch (Exception e) {
+			// Do nothing
+		} finally {
+			System.out.println("====================");
+			System.out.println("Grade for this assignment: " + grade + "%");
 		}
-		
-		System.out.println("====================");
-		System.out.println("Grade for this assignment: " + grade + "%");
 	}
 
 	public static void main(String[] args) {
